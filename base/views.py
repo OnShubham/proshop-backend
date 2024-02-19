a258870a286c97d43b29e3b38f1be43b70cefb44
OnShubham <shubhamkharche3005@gmail.com>
2024-02-19 17:17:42 +0530

    backend are config to work

---
+++ base/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+from django.http import JsonResponse
+from .products import products
+
+# Create your views here.
+
+
+def getRoutes(request):
+
+    routes = [
+    ]
+
+    return JsonResponse('khsedk', safe=False)
+
+def getProducts(request):
+    return JsonResponse(products, safe=False)
